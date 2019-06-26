794a95f6b2db782fe4e68e338620f08d6bac4467
Chase Miller <chasemillers@outlook.com>
2019-06-26 10:16:42 -0700

    added users and comments

--- src/app.js
+++ src/app.js
@@ -4,37 +4,20 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
-const ArticlesService = require('./articles-service')
-
+const articlesRouter = require('./articles/articles-router')
+const usersRouter = require('./users/users-router')
+const commentsRouter = require('./comments/comments-router')
 const app = express()
 
-app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common'))
+app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
+  skip: () => NODE_ENV === 'test'
+}))
 app.use(cors())
 app.use(helmet())
 
-app.get('/articles', (req, res, next) => {
-  const knexInstance = req.app.get('db')
-  ArticlesService.getAllArticles(knexInstance)
-    .then(articles => {
-      res.json(articles)
-    })
-    .catch(next)
-})
-
-app.get('/articles/:article_id', (req, res, next) => {
-  const knexInstance = req.app.get('db')
-  ArticlesService.getById(knexInstance, req.params.article_id)
-    .then(article => {
-      res.json(articles.map(article => ({
-        id: article.id,
-        title: article.title,
-        style: article.style,
-        content: article.content,
-        date_published: new Date(article.date_published),
-        })))
-    })
-    .catch(next)
-})
+app.use('/api/comments', commentsRouter)
+app.use('/api/articles', articlesRouter)
+app.use('/api/users', usersRouter)
 
 app.get('/', (req, res) => {
   res.send('Hello, world!')
@@ -50,5 +33,5 @@ app.use(function errorHandler(error, req, res, next) {
   }
   res.status(500).json(response)
 })
-
+ 
 module.exports = app
